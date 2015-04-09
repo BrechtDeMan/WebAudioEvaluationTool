c83d4e2de75683494b13b23c104b394803cbd456
Dave Moffat <me@davemoffat.com>
2015-04-09 12:59:46 +0100

    Creation of experimental variables and control methods document

---
+++ ape.js
@@ -0,0 +1,226 @@
+/**
+ *  ape.js
+ *  Create the APE interface
+ */
+
+// Once this is loaded and parsed, begin execution
+loadInterface(projectXML);
+
+function loadInterface(xmlDoc) {
+	
+	// Get the dimensions of the screen available to the page
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	
+	// Set background to grey #ddd
+	document.getElementsByTagName('body')[0].style.backgroundColor = '#ddd';
+	
+	// The injection point into the HTML page
+	var insertPoint = document.getElementById("topLevelBody");
+	
+	
+	// Decode parts of the xmlDoc that are needed
+	// xmlDoc MUST already be parsed by jQuery!
+	var xmlSetup = xmlDoc.find('setup');
+	// Should put in an error function here incase of malprocessed or malformed XML
+	
+	// Create the top div for the Title element
+	var titleAttr = xmlSetup[0].attributes['title'];
+	var title = document.createElement('div');
+	title.className = "title";
+	title.align = "center";
+	var titleSpan = document.createElement('span');
+	
+	// Set title to that defined in XML, else set to default
+	if (titleAttr != undefined) {
+		titleSpan.innerText = titleAttr.value;
+	} else {
+		titleSpan.innerText =  'APE Tool';
+	}
+	// Insert the titleSpan element into the title div element.
+	title.appendChild(titleSpan);
+	
+	// Store the return URL path in global projectReturn
+	projectReturn = xmlSetup[0].attributes['projectReturn'].value;
+	
+	// Create Interface buttons!
+	var interfaceButtons = document.createElement('div');
+	interfaceButtons.id = 'interface-buttons';
+	
+	// MANUAL DOWNLOAD POINT
+	// If project return is null, this MUST be specified as the location to create the download link
+	var downloadPoint = document.createElement('div');
+	downloadPoint.id = 'download-point';
+	
+	// Create playback start/stop points
+	var playback = document.createElement("button");
+	playback.innerText = 'Start';
+	// onclick function. Check if it is playing or not, call the correct function in the
+	// audioEngine, change the button text to reflect the next state.
+	playback.onclick = function() {
+		if (audioEngineContext.status == 0) {
+			audioEngineContext.play();
+			this.innerText = 'Stop';
+		} else {
+			audioEngineContext.stop();
+			this.innerText = 'Start';
+		}
+	};
+	// Create Submit (save) button
+	var submit = document.createElement("button");
+	submit.innerText = 'Submit';
+	submit.onclick = function() {
+		// TODO: Update this for postTest tags
+		createProjectSave(projectReturn)
+	};
+	// Append the interface buttons into the interfaceButtons object.
+	interfaceButtons.appendChild(playback);
+	interfaceButtons.appendChild(submit);
+	interfaceButtons.appendChild(downloadPoint);
+	
+	// Now create the slider and HTML5 canvas boxes
+	
+	// Create the div box to center align
+	var sliderBox = document.createElement('div');
+	sliderBox.className = 'sliderCanvasDiv';
+	sliderBox.id = 'sliderCanvasHolder';
+	sliderBox.align = 'center';
+	
+	// Create the slider box to hold the slider elements
+	var canvas = document.createElement('div');
+	canvas.id = 'slider';
+	// Must have a known EXACT width, as this is used later to determine the ratings
+	canvas.style.width = width - 100 +"px";
+	canvas.style.height = 150 + "px";
+	canvas.style.marginBottom = "25px";
+	canvas.style.backgroundColor = '#eee';
+	canvas.align = "left";
+	sliderBox.appendChild(canvas);
+	
+	// Global parent for the comment boxes on the page
+	var feedbackHolder = document.createElement('div');
+	// Find the parent audioHolder object.
+	var audioHolder = xmlDoc.find('audioHolder');
+	audioHolder = audioHolder[0]; // Remove from one field array
+	// Extract the hostURL attribute. If not set, create an empty string.
+	var hostURL = audioHolder.attributes['hostURL'];
+	if (hostURL == undefined) {
+		hostURL = "";
+	} else {
+		hostURL = hostURL.value;
+	}
+	// Extract the sampleRate. If set, convert the string to a Number.
+	var hostFs = audioHolder.attributes['sampleRate'];
+	if (hostFs != undefined) {
+		hostFs = Number(hostFs.value);
+	}
+	
+	/// CHECK FOR SAMPLE RATE COMPATIBILITY
+	if (hostFs != undefined) {
+		if (Number(hostFs) != audioContext.sampleRate) {
+			var errStr = 'Sample rates do not match! Requested '+Number(hostFs)+', got '+audioContext.sampleRate+'. Please set the sample rate to match before completing this test.';
+			alert(errStr);
+			return;
+		}
+	}
+	// Find all the audioElements from the audioHolder
+	var audioElements = $(audioHolder).find('audioElements');
+	audioElements.each(function(index,element){
+		// Find URL of track
+		// In this jQuery loop, variable 'this' holds the current audioElement.
+		
+		// Now load each audio sample. First create the new track by passing the full URL
+		var trackURL = hostURL + this.attributes['url'].value;
+		audioEngineContext.newTrack(trackURL);
+		// Create document objects to hold the comment boxes
+		var trackComment = document.createElement('div');
+		// Create a string next to each comment asking for a comment
+		var trackString = document.createElement('span');
+		trackString.innerText = 'Comment on track '+index;
+		// Create the HTML5 comment box 'textarea'
+		var trackCommentBox = document.createElement('textarea');
+		trackCommentBox.rows = '4';
+		trackCommentBox.cols = '100';
+		trackCommentBox.name = 'trackComment'+index;
+		trackCommentBox.className = 'trackComment';
+		// Add to the holder.
+		trackComment.appendChild(trackString);
+		trackComment.appendChild(trackCommentBox);
+		feedbackHolder.appendChild(trackComment);
+		
+		// Create a slider per track
+		
+		var trackSliderObj = document.createElement('div');
+		trackSliderObj.className = 'track-slider';
+		trackSliderObj.id = 'track-slider-'+index;
+		trackSliderObj.style.position = 'absolute';
+		// Distribute it randomnly
+		var w = window.innerWidth - 100;
+		w = Math.random()*w;
+		trackSliderObj.style.left = Math.floor(w)+50+'px';
+		trackSliderObj.style.height = "150px";
+		trackSliderObj.style.width = "10px";
+		trackSliderObj.style.backgroundColor = 'rgb(100,200,100)';
+		trackSliderObj.innerHTML = '<span>'+index+'</span>';
+		trackSliderObj.style.float = "left";
+		trackSliderObj.draggable = true;
+		trackSliderObj.ondragend = dragEnd;
+		
+		// Onclick, switch playback to that track
+		trackSliderObj.onclick = function() {
+			// Get the track ID from the object ID
+			var id = Number(this.id.substr(13,2)); // Maximum theoretical tracks is 99!
+			audioEngineContext.selectedTrack(id);
+		};
+		
+		canvas.appendChild(trackSliderObj);
+	});
+	
+	
+	// Inject into HTML
+	insertPoint.innerHTML = null; // Clear the current schema
+	insertPoint.appendChild(title); // Insert the title
+	insertPoint.appendChild(interfaceButtons);
+	insertPoint.appendChild(sliderBox);
+	insertPoint.appendChild(feedbackHolder);
+}
+
+function dragEnd(ev) {
+	// Function call when a div has been dropped
+	if (ev.x >= 50 && ev.x < window.innerWidth-50) {
+		this.style.left = (ev.x)+'px';
+	} else {
+		if (ev.x<50) {
+			this.style.left = '50px';
+		} else {
+			this.style.left = window.innerWidth-50 + 'px';
+		}
+	}
+}
+
+// Only other global function which must be defined in the interface class. Determines how to create the XML document.
+function interfaceXMLSave(){
+	// Create the XML string to be exported with results
+	var xmlDoc = document.createElement("BrowserEvaluationResult");
+	var trackSliderObjects = document.getElementsByClassName('track-slider');
+	var commentObjects = document.getElementsByClassName('trackComment');
+	var rateMin = 50;
+	var rateMax = window.innerWidth-50;
+	for (var i=0; i<trackSliderObjects.length; i++)
+	{
+		var trackObj = document.createElement("Track");
+		trackObj.id = i;
+		var slider = document.createElement("Rating");
+		var rate = Number(trackSliderObjects[i].style.left.substr(0,trackSliderObjects[i].style.left.length-2));
+		rate = (rate-rateMin)/rateMax;
+		slider.innerText = Math.floor(rate*100);
+		var comment = document.createElement("Comment");
+		comment.innerText = commentObjects[i].value;
+		trackObj.appendChild(slider);
+		trackObj.appendChild(comment);
+		xmlDoc.appendChild(trackObj);
+	}
+	
+	return xmlDoc;
+}
+
---
+++ core.js
@@ -0,0 +1,210 @@
+/**
+ * core.js
+ * 
+ * Main script to run, calls all other core functions and manages loading/store to backend.
+ * Also contains all global variables.
+ */
+
+/* create the web audio API context and store in audioContext*/
+var audioContext;
+var projectXML;
+var audioEngineContext;
+var projectReturn;
+
+window.onload = function() {
+	// Function called once the browser has loaded all files.
+	// This should perform any initial commands such as structure / loading documents
+	
+	// Create a web audio API context
+	// NORE: Currently this will only work with webkit browsers (Chrome/Safari)!
+	audioContext = new AudioContext;
+	
+	// Create the audio engine object
+	audioEngineContext = new AudioEngine();
+};
+
+function loadProjectSpec(url) {
+	// Load the project document from the given URL, decode the XML and instruct audioEngine to get audio data
+	// If url is null, request client to upload project XML document
+	var r = new XMLHttpRequest();
+	r.open('GET',url,true);
+	r.onload = function() {
+		loadProjectSpecCallback(r.response);
+	};
+	r.send();
+};
+
+function loadProjectSpecCallback(response) {
+	// Function called after asynchronous download of XML project specification
+	var decode = $.parseXML(response);
+	projectXML = $(decode);
+	
+	// Now extract the setup tag
+	var xmlSetup = projectXML.find('setup');
+	// Detect the interface to use and load the relevant javascripts.
+	var interfaceType = xmlSetup[0].attributes['interface'];
+	var interfaceJS = document.createElement('script');
+	interfaceJS.setAttribute("type","text/javascript");
+	if (interfaceType.value == 'APE') {
+		interfaceJS.setAttribute("src","ape.js");
+	}
+	document.getElementsByTagName("head")[0].appendChild(interfaceJS);
+}
+
+function createProjectSave(destURL) {
+	// Save the data from interface into XML and send to destURL
+	// If destURL is null then download XML in client
+	// Now time to render file locally
+	var xmlDoc = interfaceXMLSave();
+	if (destURL == "null" || destURL == undefined) {
+		var parent = document.createElement("div");
+		parent.appendChild(xmlDoc);
+		var file = [parent.innerHTML];
+		var bb = new Blob(file,{type : 'application/xml'});
+		var dnlk = window.URL.createObjectURL(bb);
+		var a = document.createElement("a");
+		a.hidden = '';
+		a.href = dnlk;
+		a.download = "save.xml";
+		a.textContent = "Save File";
+		
+		var submitDiv = document.getElementById('download-point');
+		submitDiv.appendChild(a);
+	}
+}
+
+function AudioEngine() {
+	
+	// Create two output paths, the main outputGain and fooGain.
+	// Output gain is default to 1 and any items for playback route here
+	// Foo gain is used for analysis to ensure paths get processed, but are not heard
+	// because web audio will optimise and any route which does not go to the destination gets ignored.
+	this.outputGain = audioContext.createGain();
+	this.fooGain = audioContext.createGain();
+	this.fooGain.gain = 0;
+	
+	// Use this to detect playback state: 0 - stopped, 1 - playing
+	this.status = 0;
+	
+	// Connect both gains to output
+	this.outputGain.connect(audioContext.destination);
+	this.fooGain.connect(audioContext.destination);
+	
+	// Create store for new audioObjects
+	this.audioObjects = [];
+	
+	this.play = function() {
+		// Send play command to all playback buffers for synchronised start
+		// Also start timer callbacks to detect if playback has finished
+		if (this.status == 0) {
+			// First get current clock
+			var timer = audioContext.currentTime;
+			// Add 3 seconds
+			timer += 3.0;
+			
+			// Send play to all tracks
+			for (var i=0; i<this.audioObjects.length; i++)
+			{
+				this.audioObjects[i].play(timer);
+			}
+			this.status = 1;
+		}
+	};
+	
+	this.stop = function() {
+		// Send stop and reset command to all playback buffers
+		if (this.status == 1) {
+			for (var i=0; i<this.audioObjects.length; i++)
+			{
+				this.audioObjects[i].stop();
+			}
+			this.status = 0;
+		}
+	};
+	
+	this.selectedTrack = function(id) {
+		for (var i=0; i<this.audioObjects.length; i++)
+		{
+			if (id == i) {
+				this.audioObjects[i].outputGain.gain.value = 1.0;
+			} else {
+				this.audioObjects[i].outputGain.gain.value = 0.0;
+			}
+		}
+	};
+	
+	
+	this.newTrack = function(url) {
+		// Pull data from given URL into new audio buffer
+		// URLs must either be from the same source OR be setup to 'Access-Control-Allow-Origin'
+		
+		// Create the audioObject with ID of the new track length;
+		audioObjectId = this.audioObjects.length
+		this.audioObjects[audioObjectId] = new audioObject(audioObjectId);
+
+		// AudioObject will get track itself.
+		this.audioObjects[audioObjectId].constructTrack(url);
+	};
+	
+}
+
+function audioObject(id) {
+	// The main buffer object with common control nodes to the AudioEngine
+	
+	this.id = id;
+	this.state = 0; // 0 - no data, 1 - ready
+	
+	// Create a buffer and external gain control to allow internal patching of effects and volume leveling.
+	this.bufferNode = audioContext.createBufferSource();
+	this.outputGain = audioContext.createGain();
+	
+	// Default output gain to be zero
+	this.outputGain.gain.value = 0.0;
+	
+	// Connect buffer to the audio graph
+	this.bufferNode.connect(this.outputGain);
+	this.outputGain.connect(audioEngineContext.outputGain);
+	
+	// the audiobuffer is not designed for multi-start playback
+	// When stopeed, the buffer node is deleted and recreated with the stored buffer.
+	this.buffer;
+	
+	this.play = function(startTime) {
+		this.bufferNode.start(startTime);
+	};
+	
+	this.stop = function() {
+		this.bufferNode.stop(0);
+		this.bufferNode = audioContext.createBufferSource();
+		this.bufferNode.connect(this.outputGain);
+		this.bufferNode.buffer = this.buffer;
+		this.bufferNode.loop = true;
+	};
+
+	this.constructTrack = function(url) {
+		var request = new XMLHttpRequest();
+		request.open('GET',url,true);
+		request.responseType = 'arraybuffer';
+		
+		var audioObj = this;
+		
+		// Create callback to decode the data asynchronously
+		request.onloadend = function() {
+			audioContext.decodeAudioData(request.response, function(decodedData) {
+				audioObj.buffer = decodedData;
+				audioObj.bufferNode.buffer = audioObj.buffer;
+				audioObj.bufferNode.loop = true;
+				audioObj.state = 1;
+			}, function(){
+				// Should only be called if there was an error, but sometimes gets called continuously
+				// Check here if the error is genuine
+				if (audioObj.state == 0 || audioObj.buffer == undefined) {
+					// Genuine error
+					console.log('FATAL - Error loading buffer on '+audioObj.id);
+				}
+			});
+		};
+		request.send();
+	};
+	
+}
