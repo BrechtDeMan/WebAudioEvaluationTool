ff7c385ba9f14996efa40c7fda2ad8e3260d0567
Nicholas Jillings <nickjillings@users.noreply.github.com>
2015-11-23 16:02:37 +0000

    Feature #1456. PHP server adds a six character 'hash' to the end of the file.

--- save.php
+++ save.php
@@ -3,7 +3,7 @@
 	header("Content-type: text/xml");
 	$postText = file_get_contents('php://input');
 	$datetime = date('ymdHis');
-	$xmlfile = "save".$datetime.".xml";
+	$xmlfile = "save".$datetime."-".generateRandomString(6).".xml";
 	$fileHandle = fopen("saves/".$xmlfile, 'w');
 	if ($fileHandle == FALSE)
 	{
@@ -25,4 +25,15 @@
 	// Return JSON confirmation data
 	$xml = '<response state="OK"><message>OK</message><file bytes="'.$wbytes.'">"saves/'.$xmlfile.'"</file></response>';
 	echo $xml;
+	
+	// Random String generator from http://stackoverflow.com/questions/4356289/php-random-string-generator
+	function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 ?>
