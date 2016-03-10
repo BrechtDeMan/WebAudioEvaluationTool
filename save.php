9a9e603e141bab02bb52413d5b6574b499cf62b3
Nicholas Jillings <n.g.r.jillings@se14.qmul.ac.uk>
2016-03-10 10:04:38 +0000

    Minor corrections to PHP save script.

--- save.php
+++ save.php
@@ -1,11 +1,5 @@
 <?php
 	error_reporting(0);
-    try{
-        date_default_timezone_get();
-    }
-    catch(Exception $e){
-        date_default_timezone_set('UTC'); // Sets to UTC if not specified anywhere in .ini
-    }
 	header('Access-Control-Allow-Origin: *');
 	header("Content-type: text/xml");
 	$postText = file_get_contents('php://input');
@@ -23,13 +17,13 @@
 	if ($wbytes === FALSE)
 	{
 		// FileWrite failed
-		$xml = '<response state="error"><message>Could not write file "saves/'.$xmlfile.'"</message></response>';
+		$xml = '<response state="error"><message>Could not write file "'.$filename.'"</message></response>';
 		echo $xml;
 		return;
 	}
 	fclose($fileHandle);
 	
-	// Return JSON confirmation data
-	$xml = '<response state="OK"><message>OK</message><file bytes="'.$wbytes.'">"saves/'.$xmlfile.'"</file></response>';
+	// Return XML confirmation data
+	$xml = '<response state="OK"><message>OK</message><file bytes="'.$wbytes.'">"'.$filename.'"</file></response>';
 	echo $xml;
 ?>
