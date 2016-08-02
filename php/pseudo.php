a064495bbadfcd66693331e1be660f30a58457a2
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2016-08-02 11:22:51 +0100

    Removed browser caching for PHP servers

--- php/pseudo.php
+++ php/pseudo.php
@@ -1,6 +1,9 @@
 <?php
     header('Access-Control-Allow-Origin: *');
 	header("Content-type: text/xml");
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
     $files = glob('../saves/' . '*.xml');
     $numsaves = 0;
     if ( $files !== false )
