6a91f64637d08c7307e1a2ccbfe1cd0616a4d0ef
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2017-11-21 15:26:50 +0000

    #257 Use ISO-8859-1 for test files

--- test_create/test_core.js
+++ test_create/test_core.js
@@ -133,7 +133,7 @@ AngularInterface.controller("view", ['$scope', '$element', '$window', function (
     $s.validate = function () {
         var s = new XMLSerializer();
         var Module = {
-            xml: s.serializeToString(specification.encode()),
+            xml: "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>" + s.serializeToString(specification.encode()),
             schema: specification.getSchemaString(),
             arguments: ["--noout", "--schema", 'test-schema.xsd', 'document.xml']
         };
