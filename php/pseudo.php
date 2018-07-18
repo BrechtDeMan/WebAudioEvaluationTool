c1c380a6d3464c645ad573210f23a38fb9acbf1e
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2018-07-18 13:56:15 +0100

    Modify the pseudo.php function to examine specific save prefixes or directory locations for tests

--- php/pseudo.php
+++ php/pseudo.php
@@ -1,24 +1,37 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
-	header("Content-type: text/xml");
-    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-    header("Cache-Control: post-check=0, pre-check=0", false);
-    header("Pragma: no-cache");
-    $files = glob('../saves/' . '*.xml');
-    $numsaves = 0;
-    if ( $files !== false )
-    {
-        $numsaves = count( $files );
-    }
+header('Access-Control-Allow-Origin: *');
+header("Content-type: text/xml");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
-    $files = glob('../tests/' . '*.xml');
-    $numtests = 0;
-    if ( $numtests !== false )
-    {
-        $numtests = count( $files );
-    }
+if (isset($_GET["prefix"]) == FALSE) {
+    $prefix = ""
+} else {
+    $prefix = $_GET["prefix"];
+}
 
-    $testID = ($numsaves % $numtests);
+if (isset($_GET["dir"]) == FALSE) {
+    $prefix = ""
+} else {
+    $prefix = $_GET["dir"];
+}
 
-	readfile($files[$testID]);
+$files = glob('../saves/' . $prefix . '*.xml');
+$numsaves = 0;
+if ( $files !== false )
+{
+    $numsaves = count( $files );
+}
+
+$files = glob('../tests/' . $dir . '*.xml');
+$numtests = 0;
+
+if ( $numtests !== false )
+{
+    $numtests = count( $files );
+}
+
+$testID = ($numsaves % $numtests);
+readfile($files[$testID]);
 ?>
