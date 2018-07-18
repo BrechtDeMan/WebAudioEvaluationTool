f674346de43cad8822aca59b8318a93606673364
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2018-07-18 17:04:53 +0100

    YAUC!

--- php/config.php
+++ php/config.php
@@ -1,14 +1,14 @@
 <?php
 function getSaveLocation() {
-    if (getenv(["WAET_SAVES_LOCATION"]) !== false) {
-        return getenv(["WAET_SAVES_LOCATION"]);
+    if (getenv("WAET_SAVES_LOCATION") !== false) {
+        return getenv("WAET_SAVES_LOCATION");
     } else {
         return "../saves/";
     }
 }
 function getTestLocation() {
-    if (getenv(["WAET_TEST_LOCATION"]) !== false) {
-        return getenv(["WAET_TEST_LOCATION"]);
+    if (getenv("WAET_TEST_LOCATION") !== false) {
+        return getenv("WAET_TEST_LOCATION");
     } else {
         return "../tests/";
     }
